bbce9ce8fadf9a4cad16ad7f94e0d25b951e2e4a
Sridhar Sarnobat <ss401533@gmail.com>
2016-08-13 21:35:43 -0700

    Minor changes (untested)

--- duff_json.groovy
+++ duff_json.groovy
@@ -1,15 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.HashSet;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+
+import org.json.JSONArray;
 
 
 public class Duff2Json {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Collection<String> blockOfFiles = new HashSet<String>();
 		while (br.ready()) {
-			String s = br.readLine();
-			System.out.println(s);
+			String inputLine = br.readLine();
+			if (inputLine.matches(".*files in cluster.*")) {
+//				System.out.println(inputLine);
+				if (blockOfFiles.size() > 0) {
+					JSONArray a = new JSONArray();
+					for (String blockFile : blockOfFiles) {
+						a.put(blockFile);
+					}
+					blockOfFiles.clear();
+					System.out.println(a.toString());
+				}
+			}
+			else {
+				blockOfFiles.add(inputLine);
+			}
 		}
 	}
 
