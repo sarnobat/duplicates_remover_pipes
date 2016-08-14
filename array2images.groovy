83daf6f18ad33f1ca31c574382c2c9864c0d642d
Sridhar Sarnobat <ss401533@gmail.com>
2016-08-13 23:30:23 -0700

    Minor changes (untested)

--- array2images.groovy
+++ array2images.groovy
@@ -2,12 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonReader;
 
 public class Array2Images {
+	private static final String BASE_URL = "http://netgear.rohidekar.com:44452/";
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,12 +20,64 @@ public class Array2Images {
 		while (!br.ready()){
 			Thread.sleep(500L);
 		}
+		for(String s : Files.readAllLines(Paths.get("./header.html"))) {
+			System.out.println(s);
+		}
 		while (br.ready()) {
 			String inputLine = br.readLine();
 			JsonArray a = jsonFromString(inputLine);
-			System.out.println("Array2Images.main()  " + a);
+			List<String> files = toCollection(a);
+			String imageTags = toImageTags(files);
+			String imageTiles = toImageTiles(files);
+			if (files.size() > 1) {
+				System.out.println(imageTiles + "<br>" + imageTags + "<br><br>");
+			}
 		}
+	}
 
+	private static String toImageTiles(List<String> filePaths) {
+		String ret = "";
+		for (String filePath : filePaths) {
+			ret += toImagePath(filePath);
+		}
+		return ret;
+
+	}
+
+	private static String toImagePath(String filePath) {
+		return getButtons(filePath)+ "<span>" + filePath + "</span><br>\n";
+	}
+
+	private static String getButtons(String filePath) {
+		if (filePath.contains("'")) {
+			System.err.println("Need to escape file paths");
+			return "";
+		}
+		if (filePath.contains("\"")) {
+			System.err.println("Need to escape file paths 2");
+			return "";
+		}
+		return "<input type=button onclick='deleteFile(\"" + filePath +"\")' value='Delete' text='myText'>";
+	}
+
+	private static String toImageTags(List<String> filePaths) {
+		String ret = "";
+		for (String filePath : filePaths) {
+			ret += toImageTag(filePath);
+		}
+		return ret;
+	}
+
+	private static String toImageTag(String filePath) {
+		return "<span id='"+filePath.replaceAll("[^A-Za-z0-9_]", "")+"'><img src='" + BASE_URL + filePath+ "' height=200></span>\n";
+	}
+
+	private static List<String> toCollection(JsonArray a) {
+		List<String> l = new LinkedList<String>();
+		for (int i = 0; i < a.size(); i++) {
+			l.add(a.getString(i));
+		}
+		return l;
 	}
 
 	private static JsonArray jsonFromString(String jsonObjectStr) {
